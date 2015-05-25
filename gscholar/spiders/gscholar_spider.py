a5f7910e027a5731494cff3790af3d0967d5f78f
Alexandr  Fonari <firstname.lastname@gaetch.edu>
2015-05-25 16:02:31 -0400

    scrape pattern for colleagues page is working

--- gscholar/spiders/gscholar_spider.py
+++ gscholar/spiders/gscholar_spider.py
@@ -1,19 +1,43 @@
-from scrapy import Spider
-from scrapy.selector import Selector
+# -*- coding: utf-8 -*-
+from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+
 from gscholar.items import GscholarItem
 
 
-class GscholarSpider(Spider):
+class GscholarSpider(CrawlSpider):
     name = "gscholar"
     allowed_domains = ["scholar.google.com"]
     start_urls = [
         "https://scholar.google.com/citations?user=Zmwh_JAAAAAJ&hl=en",
     ]
 
-    def parse(self, response):
-        names = Selector(response).xpath('//div[@id="gsc_prf_in"]')
+    rules = [
+        Rule(LinkExtractor(allow=r'citations\?view_op=list_colleagues'),
+         callback='parse_colleagues', follow=True)
+    ]
+
+    def parse_x(self, response):
+        names = response.xpath('//div[@id="gsc_prf_in"]')
 
         for n in names:
             name = GscholarItem()
             name['name'] = n.xpath('//div[@id="gsc_prf_in"]/text()').extract()[0]
             yield name
+    #
+    def parse_colleagues(self, response):
+
+        colleagues = response.xpath('//div[@class="gsc_1usr gs_scl"]')
+        #
+        items = []
+        #
+        for colleague in colleagues:
+            item = GscholarItem()
+            #
+            item['name'] = colleague.xpath(
+                'div[@class="gsc_1usr_text"]/h3[@class="gsc_1usr_name"]/a/text()').extract()[0]
+            #item['title'] = question.xpath(
+            #    'a[@class="question-hyperlink"]/text()').extract()[0]
+            items.append(item)
+        #
+        return items
