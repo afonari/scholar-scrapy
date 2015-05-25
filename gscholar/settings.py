e2b2bc433d9654065e838239b8c75a407de78f7c
Alexandr  Fonari <firstname.lastname@gaetch.edu>
2015-05-25 13:44:22 -0400

    add scrapy app: gscholar

---
+++ gscholar/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for gscholar project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'gscholar'
+
+SPIDER_MODULES = ['gscholar.spiders']
+NEWSPIDER_MODULE = 'gscholar.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'gscholar (+http://www.yourdomain.com)'
