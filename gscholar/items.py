5c0b54637db09d69b4ffdb34ac88a3545431e762
Alexandr  Fonari <firstname.lastname@gaetch.edu>
2015-05-26 09:52:04 -0400

    preparing to parse colleagues of colleagues ;)

--- gscholar/items.py
+++ gscholar/items.py
@@ -6,10 +6,14 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 from scrapy import Item, Field
-
+import re
 
 class GscholarItem(Item):
     # define the fields for your item here like:
 
+    id = Field()
     name = Field()
     email_domain = Field()
+    #
+    def set_id_from_url(self, url):
+        self['id'] = re.search(r'user=([\w\-]+)', url).group(1)
--- gscholar/spiders/gscholar_spider.py
+++ gscholar/spiders/gscholar_spider.py
@@ -1,50 +1,47 @@
 # -*- coding: utf-8 -*-
-from scrapy.contrib.linkextractors import LinkExtractor
-from scrapy.contrib.spiders import CrawlSpider, Rule
+#from scrapy.contrib.linkextractors import LinkExtractor
+#from scrapy.contrib.spiders import CrawlSpider, Rule
 
+from scrapy import Spider, Request
 from gscholar.items import GscholarItem
 import re
 
-class GscholarSpider(CrawlSpider):
-    name = "gscholar"
-    allowed_domains = ["scholar.google.com"]
+class GscholarSpider(Spider):
+    name = 'gscholar'
+    allowed_domains = ['scholar.google.com']
     start_urls = [
-        "https://scholar.google.com/citations?user=Zmwh_JAAAAAJ&hl=en",
+        'https://scholar.google.com/citations?user=Zmwh_JAAAAAJ&hl=en',
     ]
 
-    rules = [
-        Rule(LinkExtractor(allow=r'view_op=list_colleagues', process_value='process_value'),
-            callback='parse_colleagues', follow=True),
-        #Rule(LinkExtractor(allow=r'citations\?user='),
-        #    follow=True)
-    ]
+    #rules = [
+    #    Rule(LinkExtractor(allow=r'view_op=list_colleagues', process_value='process_value'),
+    #        callback='parse_colleagues', follow=True),
+    #    #Rule(LinkExtractor(allow=r'citations\?user='),
+    #    #    follow=True)
+    #]
     #
-    def process_value(self, value):
-        self.log('[SASHA]: Hi, this is an item page! %s' % value)
-        if "citations?user=" in value:
-            self.log('[SASHA]: Hi, this is an item page! %s' % value)
-            return "value" + "&view_op=list_colleagues"
-        else:
-            return None
-    #
-    def process_links(self, links):
-        for link in links:
-            link.url = "%s&view_op=list_colleagues" % link.url
+
+    def parse(self, response):
+        # self.log('SASHA: response url: %s' % response.url)
+        if re.search('\/citations\?user=[\w]+&hl=[\w]+$', response.url):
+            self.log('SASHA: user_type url: %s' % response.url)
+            redirect_url = response.url + '&view_op=list_colleagues'
+            yield Request(redirect_url, callback=self.parse)
+            return
         #
-        #print links
-        return links
-    #
+        elif re.search('&view_op=list_colleagues', response.url):
+            self.log('SASHA: colleague_list_type url: %s' % response.url)
+            #
+            # this def yields Requests for each colleague
+            colleagues = self.parse_colleagues(response)
 
-    def parse_x(self, response):
-        names = response.xpath('//div[@id="gsc_prf_in"]')
-
-        for n in names:
-            name = GscholarItem()
-            name['name'] = n.xpath('//div[@id="gsc_prf_in"]/text()').extract()[0]
-            yield name
+            for colleague in colleagues:
+                redirect_url = 'https://scholar.google.com/citations?user=' + colleague['id'] + '&hl=en&view_op=list_colleagues'
+                self.log('SASHA: requesting another list_colleague url: %s ' % redirect_url)
+                #yield Request(redirect_url, callback=self.parse)
     #
     def parse_colleagues(self, response):
-
+        self.log('SASHA: Entered parse_colleagues')
         colleagues = response.xpath('//div[@class="gsc_1usr gs_scl"]')
         #
         items = []
@@ -55,13 +52,17 @@ class GscholarSpider(CrawlSpider):
             item['name'] = colleague.xpath(
                 'div[@class="gsc_1usr_text"]/h3[@class="gsc_1usr_name"]/a/text()').extract()[0]
             #
+            user_url = colleague.xpath(
+                'div[@class="gsc_1usr_text"]/h3[@class="gsc_1usr_name"]/a/@href').extract()[0]
+            #self.log('SASHA user_url: %s' % user_url)
+            item.set_id_from_url(user_url)
+            #
             item['email_domain'] = colleague.xpath(
                 'div[@class="gsc_1usr_text"]/div[@class="gsc_1usr_emlb"]/text()').extract()[0]
             item['email_domain'] = item['email_domain'].strip().replace("@","")
             #
-
-            #item['title'] = question.xpath(
-            #    'a[@class="question-hyperlink"]/text()').extract()[0]
             items.append(item)
+            #print item
+            #
         #
         return items
