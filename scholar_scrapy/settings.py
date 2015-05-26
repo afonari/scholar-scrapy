233de9b44b1f087bd97cf74a9364abbf90951627
Alexandr  Fonari <firstname.lastname@gaetch.edu>
2015-05-26 14:37:23 -0400

    add mysql to django conf and reqs

--- scholar_scrapy/settings.py
+++ scholar_scrapy/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gscholar',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -74,13 +75,22 @@ WSGI_APPLICATION = 'scholar_scrapy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE'   : 'django.db.backends.mysql',
+        'NAME'     : 'scholar_scrapy',
+        'USER'     : 'scholar_scrapy',
+        'PASSWORD' : 'pass',
+        'HOST'     : 'localhost',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
